38c01440f27c01ba572b24ec36bd3c89aed7831e
sudura <13711345+sudura@users.noreply.github.com>
2022-11-09 22:14:07 +0000

    Fixes

--- src/main/java/me/sudura/spawnerfix/listeners/BlockListener.java
+++ src/main/java/me/sudura/spawnerfix/listeners/BlockListener.java
@@ -23,7 +23,9 @@ public class BlockListener implements Listener {
     @EventHandler
     public void onBreakBlock (BlockBreakEvent event) {
         for (String mat : plugin.getConfig().getStringList("material")){
+            plugin.getLogger().info(mat);
             Material mat2 = Material.getMaterial(mat);
+            plugin.getLogger().info(mat2.name());
             if (event.getBlock().getType() == mat2) {
                 event.setCancelled(true);
                 event.getPlayer().sendMessage(plugin.getMessages().getString("prevent.break").replace("%block%", mat.toLowerCase()));
@@ -37,7 +39,9 @@ public class BlockListener implements Listener {
         List<Block> blockList = event.blockList();
         for (Block block : blockList) {
             for (String mat : plugin.getConfig().getStringList("material")){
+                plugin.getLogger().info(mat);
                 Material mat2 = Material.getMaterial(mat);
+                plugin.getLogger().info(mat2.name());
                 if (block.getType() == mat2) {
                     event.setCancelled(true);
                     return;
@@ -64,15 +68,17 @@ public class BlockListener implements Listener {
     public void onGrow(StructureGrowEvent event) {
         if (event.getSpecies() == TreeType.RED_MUSHROOM) {
             Location loc = event.getLocation();
-
             for(int x = -2; x <= 2; ++x) {
                 for(int y = 0; y <= 9; ++y) {
                     for(int z = -2; z <= 2; ++z) {
-                        Material mat = loc.getWorld().getBlockAt(loc.getBlockX() + x, loc.getBlockY() + y, loc.getBlockZ() + z).getType();
-                        if (mat == Material.SPAWNER) {
-                            event.setCancelled(true);
-                            event.getPlayer().sendMessage(plugin.getMessages().getString("prevent.grow"));
-                            return;
+                        Material blockMat = loc.getWorld().getBlockAt(loc.getBlockX() + x, loc.getBlockY() + y, loc.getBlockZ() + z).getType();
+                        for (String mat : plugin.getConfig().getStringList("material")){
+                            Material mat2 = Material.getMaterial(mat);
+                            if (blockMat == mat2) {
+                                event.setCancelled(true);
+                                event.getPlayer().sendMessage(plugin.getMessages().getString("prevent.grow").replace("%block%", mat.toLowerCase()));
+                                return;
+                            }
                         }
                     }
                 }
